dc2aa46787ca93b5b543f0099226cd7c23b72cc5
Xiancong <xiancong88@gmail.com>
2025-05-29 16:16:29 +0800

    Create main.py
    This is the main file where the script is executed. It loads the config from the config file based on the filepath specified in the environment and runs the crawler based on the params provided in the config file. The sample config file can be found in this repo called pg_config.yaml. 
    
    The output of the crawler is a list of properties that fit the criteria specified in the params in the config file. It will then check against the output csv specified. If there is no such file a new output csv will be created and the properties populated there. If there is an output csv file with data already existing, this will help the script identify which properties are new or which have been observed before in previous crawls
    
    As this is a live crawl, please respect the robots.txt and do not abuse the script.

---
+++ main.py
@@ -0,0 +1,28 @@
+from crawler.config import load_config, get_config
+from crawler.crawler import PropertyGuruCrawler
+import os
+import pandas as pd
+
+def main():
+    config = load_config()
+
+    csv_path = get_config(config,"csv_path","Please provide the path to the output CSV")
+    api_url = get_config(config,"api_url","Please provide the API endpoint")
+    headers = get_config(config,"headers","No headers provided",True)
+    base_params = [[k,v] for k, v in get_config(config,"base_params","Base params not provided",True).items()]
+    map_areas = get_config(config,"map_areas","No coordinates provided",True)
+
+    crawler = PropertyGuruCrawler()
+    crawler.crawl()
+
+    if os.path.exists(csv_path):
+        existing_df = pd.read_csv(csv_path)
+    else:
+        existing_df = pd.DataFrame(columns=['property ID', 'date'])
+
+    new_properties = crawler.detect_new_properties(existing_df)
+    crawler.save_properties(new_properties, existing_df)
+    crawler.print_new_links(new_properties)
+
+if __name__ == "__main__":
+    main()
